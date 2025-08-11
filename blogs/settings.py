a1960ba969cb68a7c987da807219f4296ff5ab4d
rky2023 <xraj2023@gmail.com>
2025-08-11 20:12:42 +0530

    vercel db

--- blogs/settings.py
+++ blogs/settings.py
@@ -105,13 +105,25 @@ WSGI_APPLICATION = 'blogs.wsgi.application'
 
 
 # Database
+import os
+import dj_database_url
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "VERCEL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 
 # Password validation
