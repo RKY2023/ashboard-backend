7cc9e5ee63c74b61eead3bb5a017dc6ffd6ca599
rky2023 <xraj2023@gmail.com>
2025-08-11 14:48:33 +0530

    validator bug

--- vendor/models.py
+++ vendor/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from django.core.validators import EmailValidator
 # Create your models here.
 class Vendor(models.Model):
     vendor_name = models.CharField(max_length=255)
     phone_no = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     email = models.EmailField(blank=True, null=True, validators=[
-        models.EmailValidator(message="Enter a valid email address.")
+        EmailValidator(message="Enter a valid email address.")
     ])
 
     def __str__(self):
