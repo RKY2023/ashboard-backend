2f2c557a84f980f929f19c0554c9655e0e3fd408
rky2023 <xraj2023@gmail.com>
2025-08-11 14:45:47 +0530

    vercel changes

--- vendor/models.py
+++ vendor/models.py
@@ -5,7 +5,9 @@ class Vendor(models.Model):
     vendor_name = models.CharField(max_length=255)
     phone_no = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True, validators=[
+        models.EmailValidator(message="Enter a valid email address.")
+    ])
 
     def __str__(self):
         return self.vendor_name
