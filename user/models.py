d2173bceead7e8798dd2c50d2b968775626db5d7
raj <xraj2023@gmail.com>
2025-08-25 13:13:03 +0530

    user login

--- user/models.py
+++ user/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class User(AbstractUser):
-    # username = models.CharField(max_length=150, unique=True)
-    # email = models.EmailField(unique=True)
-    # password = models.CharField(max_length=128)
-    # date_joined = models.DateTimeField(auto_now_add=True)
+    username = models.CharField(max_length=150,unique=True,null=True,blank=True)
+    mobile = models.CharField(max_length=15,blank=True , null=True, validators=[
+            RegexValidator(
+                regex=r'^\+?\d{0,3}?\d{9,15}$',
+                message="Phone no must be in 10 digits. Add country code if needed.",
+            ),
+        ])
+    date_of_birth = models.DateField(blank=True,null=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_user = models.BooleanField(default=True)
+    is_staff = models.BooleanField(
+        default=False,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey('self', on_delete=models.PROTECT, related_name='created_by_parent', blank=True, null=True, default=None)
+    address = models.CharField(max_length=500,blank=True,null=True)
+    mobile_verified = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
     def __str__(self):
--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, LoginView, LogoutView
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'users', UserView, basename='user')
 urlpatterns = [
+    path('', UserView.as_view(), name='user-list'),
     path('users/', UserView.as_view(), name='user-list'),
-    # path('users/create/', UserView.as_view(), name='user-create'),
+    path('login/', LoginView.as_view(), name='user-login'),
+    path('logout/', LogoutView.as_view(), name='user-logout'),
 ]
