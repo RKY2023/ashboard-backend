4e40390b754a27c32f5a8760e9c8b2ec5963b7e1
Raj <xraj2023@gmail.com>
2025-10-02 14:25:27 +0530

    CHNAGES: State model changes

--- state/models.py
+++ state/models.py
@@ -3,9 +3,14 @@ from country.models import Country
 
 # Create your models here.
 class State(models.Model):
-    state_name = models.CharField(max_length=100)
     state_name = models.CharField(max_length=100, verbose_name='State Name', unique=True)
     state_code = models.CharField(max_length=20, verbose_name='State Code')
     country = models.ForeignKey(Country, on_delete=models.PROTECT, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "State"
+        verbose_name_plural = "States"
+        # ordering = ['name']
+
     def __str__(self):
-        return self.state_name
+        return f"{self.state_name} ({self.state_code})"
